230514ceb8aa7a08108188c6ddf0e8ad0f45cf77
JohannesL2 <johannes.lindgren2002@gmail.com>
2025-10-31 15:49:01 +0100

    fallback if no meals are found

--- src/components/MealList.jsx
+++ src/components/MealList.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 import MealCard from './MealCard'
 
 const MealList = ({meals = []}) => {
+  if (!meals.length) {
+    return (
+      <p className='text-gray-500 text-center py-6'>
+        No meals found. Try searching for something else!
+      </p>
+    )
+  }
+
   return (
     <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-6'>
     {meals?.map((meal) => (
